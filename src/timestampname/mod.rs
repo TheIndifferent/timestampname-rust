411e400e6f5789df20d2add9ee52a64d3d9ca89d
Stanislav Baiduzhyi <baiduzhyi.devel@gmail.com>
2019-02-02 10:46:02 +0100

    Listing files.

--- src/timestampname/mod.rs
+++ src/timestampname/mod.rs
@@ -1,4 +1,6 @@
 use std::path::PathBuf;
+use std::path::Path;
+use crate::timestampname::failures::Failure;
 
 pub mod failures;
 
@@ -6,10 +8,28 @@ pub struct CommandLineArguments {
     pub dry_run: bool,
     pub no_prefix: bool,
     pub debug_output: bool,
-    pub utc: bool
+    pub utc: bool,
 }
 
 pub fn execute(cwd: PathBuf, cmd_args: CommandLineArguments) -> Result<(), failures::Failure> {
     println!("path: {}, args: {}", cwd.to_str().unwrap(), cmd_args.debug_output);
-    return Ok(())
+    return Ok(());
+}
+
+fn list_files(cwd: PathBuf) -> Result<Vec<PathBuf>, Failure> {
+    let res: Result<Vec<PathBuf>, Failure> = cwd.read_dir()
+        .and_then(|iterator| {
+            let mut vec: Vec<PathBuf> = Vec::new();
+            for entry in iterator {
+                let entry = entry?;
+                if entry.file_type()?.is_dir() {
+                    continue;
+                }
+                vec.push(entry.path());
+            }
+            return Ok(vec);
+        })
+        .map_err(|e|
+            Failure::env_failure_caused("Failed to list directory contents".to_string(), e));
+    return res;
 }
